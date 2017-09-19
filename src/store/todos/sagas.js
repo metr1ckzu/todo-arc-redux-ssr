556c373b1c9fff673025f3e0068303a9b550b070
metr1ckzu <metr1ckzu@gmail.com>
2017-09-19 17:45:46 +0600

    added saga for fetching data

--- src/store/todos/sagas.js
+++ src/store/todos/sagas.js
@@ -1,16 +1,36 @@
 import { call, put, take, takeEvery, fork } from 'redux-saga/effects'
 import axios from 'axios'
-import { fetchTodosSuccess } from './actions'
+import { fetchTodosSuccess, addTodoSuccess } from './actions'
 
 
 export function* fetchTodos() {
-  const fetchedData = yield call(axios.get('http://localhost:3002/todos')
-    .then((response) => {
-      console.log('axios', response.data)
-      return response.data
-    }))
-  yield put(fetchedTodosSuccess(fetchedData))
+  const apiResponse = yield call(
+    axios.get,
+    "http://localhost:3002/todos"
+  );
 
+  yield put(
+    fetchTodosSuccess(
+      apiResponse.data
+    )
+  );
+}
+
+export function* addTodo(text) {
+  const apiResponse = yield call(
+    axios.post,
+    {
+      method: 'POST',
+      url: 'http://localhost:3002/todos/',
+      data: {
+        text: text
+      }
+    }
+  )
+
+  yield put(
+    addTodoSuccess(text, apiResponse.data.id)
+  )
 }
 
 
@@ -21,6 +41,14 @@ export function* fetchTodosRequest() {
   }
 }
 
+export function* addTodoRequest() {
+  while (true) {
+    const { text } = yield take('ADD_TODO_REQUEST')
+    yield call(addTodo, text)
+  }
+}
+
 export default function* () {
   yield fork(fetchTodosRequest)
+  yield fork(addTodoRequest)
 }
