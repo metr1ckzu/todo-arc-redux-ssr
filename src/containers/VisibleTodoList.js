d6eaaa132a77d40c0d94e4aaee1b8454461de9bd
metr1ckzu <metr1ckzu@gmail.com>
2017-09-22 16:09:00 +0600

    implementing Server Side Rendering

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,36 +1,9 @@
-import { connect } from 'react-redux'
-import { deleteTodoRequest, fetchTodosRequest } from '../store/actions'
-import TodoList from '../components/TodoList'
-import { fetchState } from 'react-router-server'
-import React from 'react'
-import { isBrowser, isServer } from 'config'
-
-const mapStateToProps = state => {
-  return {
-    todos: state.todos
-  }
-}
-
-const mapDispatchToProps = {
-  onTodoClick: deleteTodoRequest
-}
-
-const VisibleTodoList = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TodoList)
-
-export default VisibleTodoList
-//
-// class VisibleTodoList extends React.Component {
-//   componentWillMount() {
-//     this.props.readAPI()
-//   }
-//
-//   render() {
-//     return <TodoList />
-//   }
-// }
+// import { connect } from 'react-redux'
+// import { deleteTodoRequest, fetchTodosRequest } from '../store/actions'
+// import TodoList from '../components/TodoList'
+// import { fetchState } from 'react-router-server'
+// import React from 'react'
+// import { isBrowser, isServer } from 'config'
 //
 // const mapStateToProps = state => {
 //   return {
@@ -38,8 +11,66 @@ export default VisibleTodoList
 //   }
 // }
 //
-// const mapDispatchToProps = (dispatch, { done }) => ({
-//   readAPI: () => dispatch(fetchTodosRequest()).then(done, done)
-// })
+// const mapDispatchToProps = {
+//   onTodoClick: deleteTodoRequest
+// }
 //
-// export default withDone(connect(mapStateToProps, mapDispatchToProps)(VisibleTodoList))
+// const VisibleTodoList = connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(TodoList)
+//
+// export default VisibleTodoList
+
+import React from 'react'
+import { connect } from 'react-redux'
+import { fetchState } from 'react-router-server'
+import { isPending, hasFailed } from 'redux-saga-thunk'
+import { fetchTodosRequest, deleteTodoRequest } from 'store/actions'
+import { isBrowser, isServer } from 'config'
+import TodoList from '../components/TodoList'
+
+class VisibleTodoList extends React.Component {
+
+  componentWillMount() {
+    const { fetchTodos, hasServerState, setServerState, cleanServerState } = this.props
+
+    if (!hasServerState) {
+      if (isServer) {
+        fetchTodos().then(setServerState, setServerState)
+      } else {
+        fetchTodos()
+      }
+    } else if (isBrowser) {
+      cleanServerState()
+    }
+  }
+
+  render() {
+    const {todos, onTodoClick} = this.props
+    return <TodoList {...{todos, onTodoClick}} />
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  onTodoClick: (id) => dispatch(deleteTodoRequest(id)),
+  fetchTodos: () => dispatch(fetchTodosRequest())
+})
+
+const withServerState = fetchState(
+  state => ({
+    hasServerState: !!state.todos,
+  }),
+  actions => ({
+    setServerState: todos => actions.done({ todos }),
+    cleanServerState: () => actions.done(),
+  })
+)
+
+export default withServerState(connect(mapStateToProps, mapDispatchToProps)(VisibleTodoList))
